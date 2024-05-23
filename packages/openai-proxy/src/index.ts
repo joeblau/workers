ce7506eac0bd3c3a30948fa0748fd09814eeaf99
Joe Blau <im@joeblau.com>
2024-05-23 10:18:00 -0700

    feat: add user agent check

--- packages/openai-proxy/src/index.ts
+++ packages/openai-proxy/src/index.ts
@@ -1,8 +1,13 @@
 export default {
 	async fetch(request: Request, env: Env) {
-		const { method, body } = request;
+		const { method, body, headers } = request;
 
-		const proxyHeaders = new Headers(request.headers);
+		const userAgent = headers.get('user-agent');
+		if (!userAgent || !userAgent.includes('Submap')) {
+			return new Response('Forbidden', { status: 403 });
+		}
+
+		const proxyHeaders = new Headers(headers);
 		proxyHeaders.set('Authorization', `Bearer ${env.OPENAI_API_KEY}`);
 
 		const proxyURL = new URL(request.url);
